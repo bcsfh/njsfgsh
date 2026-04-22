dc179c70709267b53950d350d576d1ef8731fc23
bcsfh <boxulator2021@gmail.com>
2026-04-21 23:13:52 -0700

    Update heh.lua

--- heh.lua
+++ heh.lua
@@ -2479,7 +2479,7 @@ c:FindFirstChild("tracker").TextLabel.Text = c.Name
 end
 if c:FindFirstChild("Lid") and c:FindFirstChild("Lid"):FindFirstChild("ProximityPrompt") and (c:FindFirstChild("Lid").Position - player.Character:FindFirstChild("HumanoidRootPart").Position).magnitude < 10 then
 notify(c.Name)
-local Time = tick() + 2.25
+local Time = tick() + 3.25
 repeat
 if c:FindFirstChild("Lid") and c:FindFirstChild("Lid"):FindFirstChild("ProximityPrompt") then
 StartInteractRemote:FireServer(c:FindFirstChild("Lid"):FindFirstChild("ProximityPrompt"))
@@ -2520,7 +2520,7 @@ c:FindFirstChild("tracker").TextLabel.Text = c.Name
 end
 if c:FindFirstChild("Lid") and c:FindFirstChild("Lid"):FindFirstChild("ProximityPrompt") and (c:FindFirstChild("Lid").Position - player.Character:FindFirstChild("HumanoidRootPart").Position).magnitude < 10 then
 notify(c.Name)
-local Time = tick() + 2.25
+local Time = tick() + 3.25
 repeat
 if c:FindFirstChild("Lid") and c:FindFirstChild("Lid"):FindFirstChild("ProximityPrompt") then
 StartInteractRemote:FireServer(c:FindFirstChild("Lid"):FindFirstChild("ProximityPrompt"))
